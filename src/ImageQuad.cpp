ed69a1a1ee105307abfcae6cc4c5a67703c4a643
Wojciech Jarosz <wjarosz@cs.dartmouth.edu>
2017-01-03 12:35:11 -0500

    randomly shifting the dither texture with each redraw to get better temporal dithering

--- src/ImageQuad.cpp
+++ src/ImageQuad.cpp
@@ -5,10 +5,16 @@
 #include <iostream>     // std::cout, std::fixed
 #include <algorithm>    // std::transform
 #include <exception>    // std::transform
+#include <random>
 
 using namespace nanogui;
 using namespace Eigen;
 using namespace std;
+
+namespace
+{
+    std::mt19937 g_rand(53);
+}
 
 ImageQuad::ImageQuad() :
     m_filename()
@@ -48,6 +54,7 @@ void ImageQuad::init()
             #version 330
             uniform sampler2D source;
             uniform sampler2D dither_texture;
+            uniform vec2 randomness;
             uniform bool dither;
             uniform float gain;
             uniform vec3 channels;
@@ -56,11 +63,26 @@ void ImageQuad::init()
             in vec2 uv;
             out vec4 out_color;
             in vec4 gl_FragCoord;
-            float toSRGB(float value)
+            vec3 linearToSRGB(vec3 color)
             {
-                if (value < 0.0031308)
-                    return 12.92 * value;
-                return 1.055 * pow(value, 0.41666) - 0.055;
+               float r = color.r < 0.0031308 ? 12.92 * color.r : 1.055 * pow(color.r, 1.0/2.4) - 0.055;
+               float g = color.g < 0.0031308 ? 12.92 * color.g : 1.055 * pow(color.g, 1.0/2.4) - 0.055;
+               float b = color.b < 0.0031308 ? 12.92 * color.b : 1.055 * pow(color.b, 1.0/2.4) - 0.055;
+               return vec3(r, g, b);
+            }
+
+            vec3 sRGBToLinear(vec3 color)
+            {
+               float r = color.r < 0.04045 ? (1.0 / 12.92) * color.r : pow((color.r + 0.055) * (1.0 / 1.055), 2.4);
+               float g = color.g < 0.04045 ? (1.0 / 12.92) * color.g : pow((color.g + 0.055) * (1.0 / 1.055), 2.4);
+               float b = color.b < 0.04045 ? (1.0 / 12.92) * color.b : pow((color.b + 0.055) * (1.0 / 1.055), 2.4);
+               return vec3(r, g, b);
+            }
+
+            // note: uniformly distributed, normalized rand, [0;1[
+            float nrand(vec2 n)
+            {
+            	return fract(sin(dot(n.xy, vec2(12.9898, 78.233)))* 43758.5453);
             }
             float randZeroMeanUniform(vec2 xy)
             {
@@ -81,11 +103,8 @@ void ImageQuad::init()
             {
                 vec4 color = texture(source, uv);
                 color.rgb *= gain;
-                if (sRGB)
-                   out_color.rgb = vec3(toSRGB(color.r), toSRGB(color.g), toSRGB(color.b));
-                else
-                   out_color.rgb = pow(color.rgb, vec3(1.0/gamma));
-                float dith = randZeroMeanTriangle(gl_FragCoord.xy);
+                out_color.rgb = sRGB ? linearToSRGB(color.rgb) : pow(color.rgb, vec3(1.0/gamma));
+                float dith = randZeroMeanTriangle(gl_FragCoord.xy + randomness);
                 out_color.rgb += dither ? vec3(dith/255.0) : vec3(0.0);
                 out_color.rgb = (channels.r == 0.0) ?
                                    (channels.g == 0.0 ? out_color.bbb : out_color.ggg) :
@@ -152,6 +171,9 @@ void ImageQuad::draw(const Matrix4f & mvp,
         m_shader->setUniform("dither_texture", 1);
         m_shader->setUniform("dither", (int)dither);
         m_shader->setUniform("sRGB", (int)sRGB);
+        Vector2f randomness(std::generate_canonical<float, 10>(g_rand)*255,
+                            std::generate_canonical<float, 10>(g_rand)*255);
+        m_shader->setUniform("randomness", randomness);
         m_shader->setUniform("modelViewProj", mvp);
         m_shader->drawIndexed(GL_TRIANGLES, 0, 2);
     }
