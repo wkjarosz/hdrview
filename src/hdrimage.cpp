3ed49be54d20c98cdb5ef1b30e08048ba244af72
Wojciech Jarosz <wkjarosz@users.noreply.github.com>
2021-06-01 23:34:08 -0400

    try to fix warnings

--- src/hdrimage.cpp
+++ src/hdrimage.cpp
@@ -129,6 +129,7 @@ Color4 HDRImage::sample(float sx, float sy, Sampler s, BorderMode mX, BorderMode
 	{
 		case NEAREST:  return nearest(sx, sy, mX, mY);
 		case BILINEAR: return bilinear(sx, sy, mX, mY);
+        case default:
 		case BICUBIC:  return bicubic(sx, sy, mX, mY);
 	}
 }
@@ -1210,6 +1211,7 @@ int wrapCoord(int p, int maxP, HDRImage::BorderMode m)
             int frac = mod(p, maxP);
             return (::abs(p) / maxP % 2 != 0) ? maxP - 1 - frac : frac;
         }
+        default:
         case HDRImage::BLACK:
             return -1;
     }
