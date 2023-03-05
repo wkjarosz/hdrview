250dcf1c23ed1968814e72562b3caa11994da1ae
Wojciech Jarosz <wkjarosz@users.noreply.github.com>
2023-03-04 19:19:11 -0500

    adding search text highlighting to command palette

--- src/commandpalette.cpp
+++ src/commandpalette.cpp
@@ -17,7 +17,208 @@
 
 using namespace std;
 
+static constexpr int MAX_MATCHES = 256;
+
 NAMESPACE_BEGIN(nanogui)
+
+class CommandPalette::PaletteItem : public MenuItem
+{
+public:
+    PaletteItem(Widget *parent, const std::string &caption = "Untitled", int button_icon = 0,
+                const std::vector<Shortcut> &s = {{0, 0}}) :
+        MenuItem(parent, caption, button_icon, s)
+    {
+        // empty
+    }
+
+    void set_matches(const uint8_t matches[], int match_count)
+    {
+        m_match_count = std::min(MAX_MATCHES, match_count);
+        memcpy(m_matches, matches, m_match_count);
+    }
+
+    virtual void draw(NVGcontext *ctx) override;
+
+protected:
+    int     m_match_count          = 0;
+    uint8_t m_matches[MAX_MATCHES] = {0};
+};
+
+void CommandPalette::PaletteItem::draw(NVGcontext *ctx)
+{
+    Widget::draw(ctx);
+
+    NVGcolor grad_top = m_theme->m_button_gradient_top_unfocused;
+    NVGcolor grad_bot = m_theme->m_button_gradient_bot_unfocused;
+
+    if (m_highlighted)
+    {
+        grad_top = m_theme->m_button_gradient_top_focused;
+        grad_bot = m_theme->m_button_gradient_bot_focused;
+    }
+
+    nvgBeginPath(ctx);
+
+    nvgRoundedRect(ctx, m_pos.x() + 1, m_pos.y() + 1.0f, m_size.x() - 2, m_size.y() - 2,
+                   m_theme->m_button_corner_radius - 1);
+
+    if (m_background_color.w() != 0)
+    {
+        nvgFillColor(ctx, Color(m_background_color[0], m_background_color[1], m_background_color[2], 1.f));
+        nvgFill(ctx);
+        if (m_pushed)
+        {
+            grad_top.a = grad_bot.a = 0.8f;
+        }
+        else
+        {
+            double v   = 1 - m_background_color.w();
+            grad_top.a = grad_bot.a = m_enabled ? v : v * .5f + .5f;
+        }
+    }
+
+    NVGpaint bg = nvgLinearGradient(ctx, m_pos.x(), m_pos.y(), m_pos.x(), m_pos.y() + m_size.y(), grad_top, grad_bot);
+
+    nvgFillPaint(ctx, bg);
+    nvgFill(ctx);
+
+    nvgBeginPath(ctx);
+    nvgStrokeWidth(ctx, 1.0f);
+    nvgRoundedRect(ctx, m_pos.x() + 0.5f, m_pos.y() + (m_pushed ? 0.5f : 1.5f), m_size.x() - 1,
+                   m_size.y() - 1 - (m_pushed ? 0.0f : 1.0f), m_theme->m_button_corner_radius);
+    nvgStrokeColor(ctx, m_theme->m_border_light);
+    nvgStroke(ctx);
+
+    nvgBeginPath(ctx);
+    nvgRoundedRect(ctx, m_pos.x() + 0.5f, m_pos.y() + 0.5f, m_size.x() - 1, m_size.y() - 2,
+                   m_theme->m_button_corner_radius);
+    nvgStrokeColor(ctx, m_theme->m_border_dark);
+    nvgStroke(ctx);
+
+    int font_size = m_font_size == -1 ? m_theme->m_button_font_size : m_font_size;
+    nvgFontSize(ctx, font_size);
+    nvgFontFace(ctx, "sans-bold");
+
+    Vector2f center = Vector2f(m_pos) + Vector2f(m_size) * 0.5f;
+    Vector2f text_pos(6, center.y() - 1);
+    NVGcolor text_color = m_text_color.w() == 0 ? m_theme->m_text_color : m_text_color;
+    if (!m_enabled)
+        text_color = m_theme->m_disabled_text_color;
+
+    auto  icon = m_icon && !m_pushed ? utf8(m_icon) : utf8(FA_CHECK);
+    float ih   = font_size * icon_scale();
+    nvgFontSize(ctx, ih);
+    nvgFontFace(ctx, "icons");
+    float iw = nvgTextBounds(ctx, 0, 0, icon.data(), nullptr, nullptr);
+
+    if (m_caption != "")
+        ih += m_size.y() * 0.15f;
+
+    nvgFillColor(ctx, text_color);
+    nvgTextAlign(ctx, NVG_ALIGN_LEFT | NVG_ALIGN_MIDDLE);
+    Vector2f icon_pos(m_pos.x() + 6, center.y() - 1);
+
+    text_pos.x() = icon_pos.x() + ih + 2;
+
+    if (m_pushed || m_icon)
+        nvgText(ctx, icon_pos.x() + (ih - iw - 3) / 2, icon_pos.y() + 1, icon.data(), nullptr);
+
+    auto draw_shadowed_text =
+        [this, &text_color, ctx, &text_pos](bool highlighted, const char *start, const char *end = nullptr)
+    {
+        nvgFontFace(ctx, highlighted ? "sans-bold" : "sans");
+        nvgFillColor(ctx, m_theme->m_text_color_shadow);
+        nvgText(ctx, text_pos.x(), text_pos.y(), start, end);
+        nvgFillColor(ctx, highlighted ? Color(1.f, 1.f, 1.f, 1.f) : text_color);
+        return nvgText(ctx, text_pos.x(), text_pos.y() + 1, start, end);
+    };
+
+    // The following code that performs the substring highlighting logic is adapted from ImGui Command Palette
+    // The MIT License (MIT)
+    //
+    // Copyright (c) 2021 hnOsmium0001
+    //
+    // Permission is hereby granted, free of charge, to any person obtaining a copy
+    // of this software and associated documentation files (the "Software"), to deal
+    // in the Software without restriction, including without limitation the rights
+    // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    // copies of the Software, and to permit persons to whom the Software is
+    // furnished to do so, subject to the following conditions:
+    //
+    // The above copyright notice and this permission notice shall be included in all
+    // copies or substantial portions of the Software.
+    //
+    // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    // SOFTWARE.
+
+    nvgFontSize(ctx, font_size);
+    nvgTextAlign(ctx, NVG_ALIGN_LEFT | NVG_ALIGN_MIDDLE);
+    if (m_match_count <= 0)
+        // draw text as-is, without highlighting matches
+        draw_shadowed_text(false, m_caption.c_str());
+    else
+    {
+        auto text = m_caption.c_str();
+        int  range_begin;
+        int  range_end;
+        int  last_range_end = 0;
+
+        auto draw_current_range = [&]()
+        {
+            if (range_begin != last_range_end)
+                // Draw normal text between last highlighted range end and current highlighted range start
+                text_pos.x() = draw_shadowed_text(false, text + last_range_end, text + range_begin);
+
+            text_pos.x() = draw_shadowed_text(true, text + range_begin, text + range_end);
+        };
+
+        range_begin = m_matches[0];
+        range_end   = range_begin;
+
+        int last_char_idx = -1;
+        for (int j = 0; j < m_match_count; ++j)
+        {
+            int char_idx = m_matches[j];
+
+            if (char_idx == last_char_idx + 1)
+                // These 2 indices are equal, extend our current range by 1
+                ++range_end;
+            else
+            {
+                draw_current_range();
+                last_range_end = range_end;
+                range_begin    = char_idx;
+                range_end      = char_idx + 1;
+            }
+
+            last_char_idx = char_idx;
+        }
+
+        // Draw the remaining range (if any)
+        if (range_begin != range_end)
+            draw_current_range();
+
+        // Draw the text after the last range (if any)
+        draw_shadowed_text(false, text + range_end);
+    }
+
+    if (!shortcut().text.size())
+        return;
+
+    // float    sw = nvgTextBounds(ctx, 0, 0, shortcut().text.c_str(), nullptr, nullptr);
+    Vector2f hotkey_pos(m_pos.x() + m_size.x() - 8, center.y() - 1);
+
+    nvgTextAlign(ctx, NVG_ALIGN_RIGHT | NVG_ALIGN_MIDDLE);
+    nvgFillColor(ctx, m_theme->m_text_color_shadow);
+    nvgText(ctx, hotkey_pos.x(), hotkey_pos.y(), shortcut().text.c_str(), nullptr);
+    nvgFillColor(ctx, m_theme->m_disabled_text_color);
+    nvgText(ctx, hotkey_pos.x(), hotkey_pos.y() + 1, shortcut().text.c_str(), nullptr);
+}
 
 CommandPalette::CommandPalette(Widget *parent, const std::vector<MenuItem *> &commands) :
     Dialog(parent, "", false), m_original_commands(commands)
@@ -48,8 +249,6 @@ CommandPalette::CommandPalette(Widget *parent, const std::vector<MenuItem *> &co
     m_search_box->set_placeholder("Filter commands...");
     m_search_box->set_tooltip("Search for commands.");
 
-    // auto well = new Well{this, 3, Color(0, 64), Color(0, 128), Color(5, 255)};
-    // auto well = new Well{this};
     auto well = new Well{this, 3, Color(0, 16), Color(0, 32), Color(0, 64)};
     well->set_layout(new BoxLayout(Orientation::Vertical, Alignment::Fill, 0));
 
@@ -94,15 +293,23 @@ CommandPalette::CommandPalette(Widget *parent, const std::vector<MenuItem *> &co
 
         struct ScoredItem
         {
-            int original_index;
-            int score;
+            int     original_index       = 0;
+            int     score                = 0;
+            int     match_count          = 0;
+            uint8_t matches[MAX_MATCHES] = {0};
         };
 
         auto all_items = [this]()
         {
             std::vector<ScoredItem> items;
             for (int i = 0; i < (int)m_original_commands.size(); ++i)
+            {
+                MenuItem *entry = m_original_commands[i];
+                if (!entry->enabled())
+                    continue;
+
                 items.emplace_back(ScoredItem{i, (int)m_original_commands.size() - i});
+            }
 
             std::sort(items.begin(), items.end(),
                       [this](auto &&a, auto &&b)
@@ -121,10 +328,20 @@ CommandPalette::CommandPalette(Widget *parent, const std::vector<MenuItem *> &co
             for (int i = 0; i < (int)m_original_commands.size(); ++i)
             {
                 MenuItem *entry = m_original_commands[i];
-                int       score = -10000000;
-                fts::fuzzy_match(pattern.c_str(), entry->caption().c_str(), score);
-                matches.emplace_back(ScoredItem{i, score});
-                spdlog::trace("Matching {} score {}", entry->caption().c_str(), score);
+                if (!entry->enabled())
+                    continue;
+
+                ScoredItem result;
+                if (fts::fuzzy_match(pattern.c_str(), entry->caption().c_str(), result.score, result.matches,
+                                     MAX_MATCHES, result.match_count))
+                {
+                    result.original_index = i;
+                    matches.push_back(result);
+
+                    spdlog::trace("Matched \"{}\" with score {} and {} matches", entry->caption().c_str(), result.score,
+                                  result.match_count);
+                    for (int j = 0; j < result.match_count; ++j) spdlog::trace("   {}", result.matches[j]);
+                }
             }
 
             std::sort(matches.begin(), matches.end(),
@@ -187,14 +404,16 @@ CommandPalette::CommandPalette(Widget *parent, const std::vector<MenuItem *> &co
         auto items = pattern.empty() ? all_items() : scored_matches(pattern);
 
         m_commands.clear();
-        for (size_t i = 0; i < items.size() && items[i].score >= 0; ++i)
+        for (const auto &i : items)
         {
-            MenuItem *orig_item = m_original_commands[items[i].original_index];
+            MenuItem *orig_item = m_original_commands[i.original_index];
             if (!orig_item->enabled())
                 continue;
             size_t j = m_commands.size();
-            spdlog::debug("adding item \"{}\" with score {}", orig_item->caption(), items[i].score);
-            auto item = new MenuItem{m_commandlist, orig_item->caption(), orig_item->icon(), {orig_item->shortcut(0)}};
+            spdlog::debug("adding item \"{}\" with score {}", orig_item->caption(), i.score);
+            auto item =
+                new PaletteItem{m_commandlist, orig_item->caption(), orig_item->icon(), {orig_item->shortcut(0)}};
+            item->set_matches(i.matches, i.match_count);
             item->set_flags(orig_item->flags());
             item->set_tooltip(orig_item->tooltip());
             item->set_callback(
@@ -244,7 +463,7 @@ CommandPalette::CommandPalette(Widget *parent, const std::vector<MenuItem *> &co
 
         if (m_commands.empty())
         {
-            auto non_found = new MenuItem{m_commandlist, "No matching commands"};
+            auto non_found = new PaletteItem{m_commandlist, "No matching commands"};
             non_found->set_enabled(false);
             m_commands.push_back(non_found);
         }
--- src/commandpalette.h
+++ src/commandpalette.h
@@ -10,6 +10,8 @@ NAMESPACE_BEGIN(nanogui)
 class CommandPalette : public Dialog
 {
 public:
+    class PaletteItem;
+
     CommandPalette(Widget *parent, const std::vector<MenuItem *> &commands = {});
 
     virtual void draw(NVGcontext *ctx) override;
@@ -18,12 +20,12 @@ public:
 protected:
     void update_geometry();
 
-    SearchBox              *m_search_box  = nullptr;
-    Widget                 *m_commandlist = nullptr;
-    VScrollPanel           *m_vscroll     = nullptr;
-    int                     m_current     = -1; ///< The currently selected item
-    std::vector<MenuItem *> m_original_commands;
-    std::vector<MenuItem *> m_commands;
+    SearchBox                 *m_search_box  = nullptr;
+    Widget                    *m_commandlist = nullptr;
+    VScrollPanel              *m_vscroll     = nullptr;
+    int                        m_current     = -1; ///< The currently selected item
+    std::vector<MenuItem *>    m_original_commands;
+    std::vector<PaletteItem *> m_commands;
 };
 
 NAMESPACE_END(nanogui)
