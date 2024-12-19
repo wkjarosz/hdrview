460f8489ff46d04b2c48f249b164fa537fa867f2
Wojciech Jarosz <wkjarosz@users.noreply.github.com>
2024-12-19 14:26:02 -0500

    creating custom style

--- src/app.cpp
+++ src/app.cpp
@@ -527,12 +527,6 @@ HDRViewApp::HDRViewApp(float exposure, float gamma, bool dither, bool sRGB, bool
         HelloImGui::DockingSplit{"ImagesSpace", "ChannelsSpace", ImGuiDir_Down, 0.25f},
         HelloImGui::DockingSplit{"MainDockSpace", "LogSpace", ImGuiDir_Down, 0.25f}};
 
-    m_params.callbacks.SetupImGuiStyle = []()
-    {
-        // make things like radio buttons look nice and round
-        ImGui::GetStyle().CircleTessellationMaxError = 0.1f;
-    };
-
 #if defined(HELLOIMGUI_USE_GLFW)
     m_params.callbacks.PostInit_AddPlatformBackendCallbacks = [this]
     {
@@ -587,6 +581,102 @@ HDRViewApp::HDRViewApp(float exposure, float gamma, bool dither, bool sRGB, bool
         setup_rendering();
     };
     m_params.callbacks.BeforeExit = [this] { save_settings(); };
+
+    // Change style
+    m_params.callbacks.SetupImGuiStyle = []()
+    {
+        // Example of theme customization at App startup
+
+        // Apply default style
+        HelloImGui::ImGuiDefaultSettings::SetupDefaultImGuiStyle();
+        // Create a tweaked theme
+        ImGuiTheme::ImGuiTweakedTheme tweakedTheme;
+        tweakedTheme.Theme           = ImGuiTheme::ImGuiTheme_DarculaDarker;
+        tweakedTheme.Tweaks.Rounding = 4.0f;
+
+        tweakedTheme.Tweaks.ValueMultiplierBg      = 0.5;
+        tweakedTheme.Tweaks.ValueMultiplierFrameBg = 0.5;
+
+        // Apply the tweaked theme
+        ImGuiTheme::ApplyTweakedTheme(tweakedTheme);
+
+        // Then apply further modifications to ImGui style
+        ImGui::GetStyle().DisabledAlpha            = 0.5;
+        ImGui::GetStyle().WindowRounding           = 0;
+        ImGui::GetStyle().WindowBorderSize         = 0.2;
+        ImGui::GetStyle().WindowMenuButtonPosition = ImGuiDir_Right;
+        ImGui::GetStyle().WindowPadding            = ImVec2(8, 8);
+        ImGui::GetStyle().FrameRounding            = 3;
+        ImGui::GetStyle().PopupRounding            = 4;
+        ImGui::GetStyle().TabRounding              = 4;
+        ImGui::GetStyle().DockingSeparatorSize     = 2;
+        ImGui::GetStyle().SeparatorTextBorderSize  = 1;
+        // ImGui::GetStyle().TabBarBorderSize         = 0;
+        // ImGui::GetStyle().FramePadding             = ImVec2(10, 8);
+
+        // make things like radio buttons look nice and round
+        ImGui::GetStyle().CircleTessellationMaxError = 0.1f;
+
+        ImVec4 *colors                             = ImGui::GetStyle().Colors;
+        colors[ImGuiCol_Text]                      = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+        colors[ImGuiCol_TextDisabled]              = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
+        colors[ImGuiCol_WindowBg]                  = ImVec4(0.20f, 0.20f, 0.20f, 1.00f);
+        colors[ImGuiCol_ChildBg]                   = ImVec4(0.04f, 0.04f, 0.04f, 0.47f);
+        colors[ImGuiCol_PopupBg]                   = ImVec4(0.18f, 0.18f, 0.18f, 1.00f);
+        colors[ImGuiCol_Border]                    = ImVec4(0.08f, 0.08f, 0.08f, 1.00f);
+        colors[ImGuiCol_BorderShadow]              = ImVec4(1.00f, 1.00f, 1.00f, 0.16f);
+        colors[ImGuiCol_FrameBg]                   = ImVec4(0.11f, 0.11f, 0.11f, 1.00f);
+        colors[ImGuiCol_FrameBgHovered]            = ImVec4(1.00f, 1.00f, 1.00f, 0.13f);
+        colors[ImGuiCol_FrameBgActive]             = ImVec4(0.28f, 0.28f, 0.28f, 1.00f);
+        colors[ImGuiCol_TitleBg]                   = ImVec4(0.13f, 0.13f, 0.13f, 0.90f);
+        colors[ImGuiCol_TitleBgActive]             = ImVec4(0.16f, 0.16f, 0.16f, 0.90f);
+        colors[ImGuiCol_TitleBgCollapsed]          = ImVec4(0.08f, 0.08f, 0.08f, 1.00f);
+        colors[ImGuiCol_MenuBarBg]                 = ImVec4(0.10f, 0.10f, 0.10f, 1.00f);
+        colors[ImGuiCol_ScrollbarBg]               = ImVec4(0.13f, 0.13f, 0.13f, 1.00f);
+        colors[ImGuiCol_ScrollbarGrab]             = ImVec4(0.28f, 0.28f, 0.28f, 1.00f);
+        colors[ImGuiCol_ScrollbarGrabHovered]      = ImVec4(0.39f, 0.39f, 0.39f, 1.00f);
+        colors[ImGuiCol_ScrollbarGrabActive]       = ImVec4(0.24f, 0.47f, 0.81f, 1.00f);
+        colors[ImGuiCol_CheckMark]                 = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+        colors[ImGuiCol_SliderGrab]                = ImVec4(0.39f, 0.39f, 0.39f, 1.00f);
+        colors[ImGuiCol_SliderGrabActive]          = ImVec4(0.24f, 0.47f, 0.81f, 1.00f);
+        colors[ImGuiCol_Button]                    = ImVec4(1.00f, 1.00f, 1.00f, 0.06f);
+        colors[ImGuiCol_ButtonHovered]             = ImVec4(1.00f, 1.00f, 1.00f, 0.13f);
+        colors[ImGuiCol_ButtonActive]              = ImVec4(0.14f, 0.18f, 0.26f, 1.00f);
+        colors[ImGuiCol_Header]                    = ImVec4(0.19f, 0.19f, 0.19f, 1.00f);
+        colors[ImGuiCol_HeaderHovered]             = ImVec4(0.24f, 0.47f, 0.81f, 1.00f);
+        colors[ImGuiCol_HeaderActive]              = ImVec4(0.47f, 0.47f, 0.47f, 1.00f);
+        colors[ImGuiCol_Separator]                 = ImVec4(0.13f, 0.13f, 0.13f, 1.00f);
+        colors[ImGuiCol_SeparatorHovered]          = ImVec4(0.39f, 0.39f, 0.39f, 1.00f);
+        colors[ImGuiCol_SeparatorActive]           = ImVec4(0.24f, 0.47f, 0.81f, 1.00f);
+        colors[ImGuiCol_ResizeGrip]                = ImVec4(1.00f, 1.00f, 1.00f, 0.25f);
+        colors[ImGuiCol_ResizeGripHovered]         = ImVec4(0.39f, 0.39f, 0.39f, 1.00f);
+        colors[ImGuiCol_ResizeGripActive]          = ImVec4(0.24f, 0.47f, 0.81f, 1.00f);
+        colors[ImGuiCol_TabHovered]                = ImVec4(0.24f, 0.47f, 0.81f, 0.78f);
+        colors[ImGuiCol_Tab]                       = ImVec4(0.10f, 0.10f, 0.10f, 1.00f);
+        colors[ImGuiCol_TabSelected]               = ImVec4(0.24f, 0.47f, 0.81f, 1.00f);
+        colors[ImGuiCol_TabSelectedOverline]       = ImVec4(0.30f, 0.58f, 1.00f, 1.00f);
+        colors[ImGuiCol_TabDimmed]                 = ImVec4(0.49f, 0.49f, 0.49f, 0.08f);
+        colors[ImGuiCol_TabDimmedSelected]         = ImVec4(0.24f, 0.47f, 0.81f, 1.00f);
+        colors[ImGuiCol_TabDimmedSelectedOverline] = ImVec4(0.30f, 0.58f, 1.00f, 1.00f);
+        colors[ImGuiCol_DockingPreview]            = ImVec4(0.24f, 0.47f, 0.81f, 1.00f);
+        colors[ImGuiCol_DockingEmptyBg]            = ImVec4(0.18f, 0.18f, 0.18f, 1.00f);
+        colors[ImGuiCol_PlotLines]                 = ImVec4(0.47f, 0.47f, 0.47f, 1.00f);
+        colors[ImGuiCol_PlotLinesHovered]          = ImVec4(1.00f, 0.39f, 0.00f, 1.00f);
+        colors[ImGuiCol_PlotHistogram]             = ImVec4(0.59f, 0.59f, 0.59f, 1.00f);
+        colors[ImGuiCol_PlotHistogramHovered]      = ImVec4(1.00f, 0.39f, 0.00f, 1.00f);
+        colors[ImGuiCol_TableHeaderBg]             = ImVec4(0.18f, 0.19f, 0.20f, 1.00f);
+        colors[ImGuiCol_TableBorderStrong]         = ImVec4(0.30f, 0.32f, 0.34f, 1.00f);
+        colors[ImGuiCol_TableBorderLight]          = ImVec4(0.22f, 0.23f, 0.24f, 1.00f);
+        colors[ImGuiCol_TableRowBg]                = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+        colors[ImGuiCol_TableRowBgAlt]             = ImVec4(0.98f, 0.98f, 0.98f, 0.06f);
+        colors[ImGuiCol_TextLink]                  = ImVec4(0.26f, 0.50f, 0.96f, 1.00f);
+        colors[ImGuiCol_TextSelectedBg]            = ImVec4(1.00f, 1.00f, 1.00f, 0.16f);
+        colors[ImGuiCol_DragDropTarget]            = ImVec4(0.24f, 0.47f, 0.81f, 1.00f);
+        colors[ImGuiCol_NavCursor]                 = ImVec4(0.24f, 0.47f, 0.81f, 1.00f);
+        colors[ImGuiCol_NavWindowingHighlight]     = ImVec4(0.24f, 0.47f, 0.81f, 1.00f);
+        colors[ImGuiCol_NavWindowingDimBg]         = ImVec4(0.00f, 0.00f, 0.00f, 0.59f);
+        colors[ImGuiCol_ModalWindowDimBg]          = ImVec4(0.00f, 0.00f, 0.00f, 0.59f);
+    };
 
     m_params.callbacks.ShowGui = [this]()
     {
