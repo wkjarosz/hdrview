45859e47e0895b6d26f71067a598d29105a4fc02
Wojciech Jarosz <wkjarosz@users.noreply.github.com>
2025-12-16 22:45:39 -0500

    Update cliformatter.h

--- src/cliformatter.h
+++ src/cliformatter.h
@@ -6,7 +6,24 @@
 
 #pragma once
 #include <CLI/CLI.hpp>
+
+//
+// include bundled or external copy of fmtlib's color support
+//
+#include <spdlog/tweakme.h>
+
+#if !defined(SPDLOG_USE_STD_FORMAT)
+#if !defined(SPDLOG_FMT_EXTERNAL)
+#ifdef SPDLOG_HEADER_ONLY
+#ifndef FMT_HEADER_ONLY
+#define FMT_HEADER_ONLY
+#endif
+#endif
 #include <spdlog/fmt/bundled/color.h>
+#else
+#include <fmt/color.h>
+#endif
+#endif
 
 class ColorFormatter : public CLI::Formatter
 {
